92c9d35cb1189002df8e091bf6abfb3e67991dc3
MaxLC1 <maxlaime617@gmail.com>
2026-09-15 09:12:53 -0400

    Configurando IP publica de AWS para produccion

--- app/pkg_archivos/router.py
+++ app/pkg_archivos/router.py
@@ -21,4 +21,4 @@ def upload_file(file: UploadFile = File(...)):
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
         
-    return {"url": f"http://192.168.100.4:8000/uploads/{unique_filename}"}
+    return {"url": f"http://34.230.18.9:8000/uploads/{unique_filename}"}
